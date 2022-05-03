c73c15e0b703b56407f1a7dd4e7d487fcadd2c38
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-03 16:33:37 +0200

    add style COMPLETE

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Movies</title>
 </head>
 <body>
 
-    <ul>
-        @foreach ($movies as $movie)
-        <li>
-            <h2>{{ $movie['title'] }}</h2>
-            <p>Vote: {{ $movie['vote'] }}</p>
-        </li>            
-        @endforeach
-    </ul>
+    <section class="movies-wrapper">
+
+        <ul class="movies-list">
+            @foreach ($movies as $movie)
+            <li class="movie-item">
+                <h2>{{ $movie['title'] }}</h2>
+                <p>Vote: {{ $movie['vote'] }}</p>
+            </li>            
+            @endforeach
+        </ul>
+    
+    </section>
     
 </body>
 </html>
